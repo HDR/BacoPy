9992ac767401a18235ce9ea0e6be2b1d2b792be5
Martin <MrHDR@users.noreply.github.com>
2017-10-08 21:12:39 +0200

    Version 1.0
    Updated to work with the latest discord api & removed need for username and password for praw.

--- baco.py
+++ baco.py
@@ -52,7 +52,6 @@ if ruseragent == '':
 
 r = praw.Reddit(client_id=rclientid,
                 client_secret=rclientsecret,
-                password=rpassword,
                 user_agent=ruseragent,
                 username=rusername)
 client = discord.Client()
@@ -67,93 +66,95 @@ cmd_gif = (commands['cmd_gif'])
 cmd_clear = (commands['cmd_clear'])
 
 
+class BacoPy(discord.Client):
+    def __init__(self):
+        super().__init__()
 
-@client.event
-async def on_ready():
-    print('\nLogin Details:')
-    print('---------------------')
-    print('Logged in as:')
-    print(client.user.name)
-    print('Bot User ID:')
-    print(client.user.id)
-    print('---------------------\n')
-    print('---------------------------------------')
-    print('Running discord.py version ' + discord.__version__)
-    print('---------------------------------------\n')
-    print('STATUS: Finished Loading!')
-    print('-------------------------\n')
-    print('-----------------------------------------')
-    print('Author: HDR')
-    print('Version: Dev 0.9')
-    print('Build Date: 2 April 2017.')
-    print('-----------------------------------------\n')
+    async def on_ready(self):
+        print('---------------------')
+        print('Logged in as:')
+        print(self.user.name)
+        print('Bot User ID:')
+        print(self.user.id)
+        print('---------------------\n')
+        print('---------------------------------------')
+        print('Running discord.py version ' + discord.__version__)
+        print('---------------------------------------\n')
+        print('-------------------------')
+        print('STATUS: Finished Loading!')
+        print('-------------------------\n')
+        print('-----------------------------------------')
+        print('Author: HDR')
+        print('Version: 1.0')
+        print('Build Date: 8 October 2017')
+        print('-----------------------------------------\n')
 
-@client.event
-async def on_message(message):
-    initiator_data = ('by ' + str(message.author) + ' ,UserID: ' + message.author.id + '. Server: ' + message.server.id)
-    client.change_status(game=None)
-    if message.content.startswith(pfx + cmd_help):
-        cmd_name = 'Help'
-        await client.send_message(message.channel, '**--stats**\n**--help**\n**--hentai**\n**--r34**\n**--nsfw**\n**--gif**')
-        print('CMD [' + pfx + cmd_name + '] > ' + initiator_data)
-    elif message.content.startswith(pfx + cmd_hentai):
-        hen = r.subreddit('hentai')
-        posts = hen.random()
-        await client.send_message(message.channel, posts.url)
-        cmd_name = 'Hentai'
-        print('CMD [' + pfx + cmd_name + '] > ' + initiator_data)
-    elif message.content.startswith(pfx + cmd_stats):
-        cmd_name = 'Stats'
-        def getCPUtemp():
-            res = os.popen('vcgencmd measure_temp').readline()
-            return(res.replace("temp=","").replace("'C\n",""))
-        CPU_temp = str(getCPUtemp())
+    async def on_message(self, message):
+        initiator_data = ('by ' + str(message.author) + ' ,UserID: ' + message.author.id + '. Server: ' + message.server.id)
+        self.change_status(game=None)
+        if message.content.startswith(pfx + cmd_help):
+            cmd_name = 'Help'
+            await self.send_message(message.channel, '**--stats**\n**--help**\n**--hentai**\n**--r34**\n**--nsfw**\n**--gif**')
+            print('CMD [' + pfx + cmd_name + '] > ' + initiator_data)
+        elif message.content.startswith(pfx + cmd_hentai):
+            hen = r.subreddit('hentai')
+            posts = hen.random()
+            await self.send_message(message.channel, posts.url)
+            cmd_name = 'Hentai'
+            print('CMD [' + pfx + cmd_name + '] > ' + initiator_data)
+        elif message.content.startswith(pfx + cmd_stats):
+            cmd_name = 'Stats'
+            def getCPUtemp():
+                res = os.popen('vcgencmd measure_temp').readline()
+                return(res.replace("temp=","").replace("'C\n",""))
+            CPU_temp = str(getCPUtemp())
 
-        def getCPUuse():
-            return(str(os.popen("top -n1 | awk '/Cpu\(s\):/ {print $2}'").readline().strip(\
-        )))
-        CPU_load = str(getCPUuse())
-        RAM_usage = str(psutil.virtual_memory().percent)
-        getruntime = round(time.time() - start_time, 10) / (60)
-        runtimeint = int(getruntime)
-        runtime = str(runtimeint)
-        await client.send_message(message.channel,
-                                  '**Runtime:**\n' + runtime + ' Minutes \n**CPU Load:** \n' + CPU_load + '% \n' + '**CPU Temp:** \n' + CPU_temp + '°C \n' + '**RAM Usage:**\n' + RAM_usage + '%')
-        print('CMD [' + pfx + cmd_name + '] > ' + initiator_data)
-    elif message.content.startswith('<@' + client.user.id + '>'):
-        cmd_name = 'BOT Mentioned'
-        await client.send_message(message.channel, 'What do you want slut?')
-        print('CMD [' + pfx + cmd_name + '] > ' + initiator_data)
-    elif message.content.startswith(pfx + cmd_r34):
-        r34 = r.subreddit('rule34')
-        posts = r34.random()
-        await client.send_message(message.channel, posts.url)
-        cmd_name = 'r34'
-        print('CMD [' + pfx + cmd_name + '] > ' + initiator_data)
-    elif message.content.startswith(pfx + cmd_nsfw):
-        nsfw = r.subreddit('nsfw')
-        posts = nsfw.random()
-        await client.send_message(message.channel, posts.url)
-        cmd_name = 'nsfw'
-        print('CMD [' + pfx + cmd_name + '] > ' + initiator_data)
-    elif message.content.startswith(pfx + cmd_gif):
-        gif = r.subreddit('gifs')
-        posts = gif.random()
-        await client.send_message(message.channel, posts.url)
-        cmd_name = 'gif'
-        print('CMD [' + pfx + cmd_name + '] > ' + initiator_data)
-    elif message.content.startswith(pfx + cmd_clear):
-        def is_me(m):
-            return m.author == client.user
-        deleted = await client.purge_from(message.channel, limit=100, check=is_me)
-        await client.send_message(message.channel, 'Deleted {} message(s)'.format(len(deleted)))
-        cmd_name = 'clear'
-        print('CMD [' + pfx + cmd_name + '] > ' + initiator_data)
-    elif message.content.startswith('debugtest'):
-        getruntime = round(time.time() - start_time, 10) / (60)
-        runtime = int(getruntime)
-        await client.send_message(message.channel, runtime)
-        await client.send_message(message.channel, commands)
-    if message.content.startswith(pfx):
-        await client.delete_message(message)
-client.run(token)
+            def getCPUuse():
+                return(str(os.popen("top -n1 | awk '/Cpu\(s\):/ {print $2}'").readline().strip(\
+            )))
+            CPU_load = str(getCPUuse())
+            RAM_usage = str(psutil.virtual_memory().percent)
+            getruntime = round(time.time() - start_time, 10) / (60)
+            runtimeint = int(getruntime)
+            runtime = str(runtimeint)
+            await self.send_message(message.channel,
+                                      '**Runtime:**\n' + runtime + ' Minutes \n**CPU Load:** \n' + CPU_load + '% \n' + '**CPU Temp:** \n' + CPU_temp + '°C \n' + '**RAM Usage:**\n' + RAM_usage + '%')
+            print('CMD [' + pfx + cmd_name + '] > ' + initiator_data)
+        elif message.content.startswith('<@' + self.user.id + '>'):
+            cmd_name = 'BOT Mentioned'
+            await self.send_message(message.channel, 'What do you want slut?')
+            print('CMD [' + pfx + cmd_name + '] > ' + initiator_data)
+        elif message.content.startswith(pfx + cmd_r34):
+            r34 = r.subreddit('rule34')
+            posts = r34.random()
+            await self.send_message(message.channel, posts.url)
+            cmd_name = 'r34'
+            print('CMD [' + pfx + cmd_name + '] > ' + initiator_data)
+        elif message.content.startswith(pfx + cmd_nsfw):
+            nsfw = r.subreddit('nsfw')
+            posts = nsfw.random()
+            await self.send_message(message.channel, posts.url)
+            cmd_name = 'nsfw'
+            print('CMD [' + pfx + cmd_name + '] > ' + initiator_data)
+        elif message.content.startswith(pfx + cmd_gif):
+            gif = r.subreddit('gifs')
+            posts = gif.random()
+            await self.send_message(message.channel, posts.url)
+            cmd_name = 'gif'
+            print('CMD [' + pfx + cmd_name + '] > ' + initiator_data)
+        elif message.content.startswith(pfx + cmd_clear):
+            def is_me(m):
+                return m.author == self.user
+            deleted = await self.purge_from(message.channel, limit=100, check=is_me)
+            await self.send_message(message.channel, 'Deleted {} message(s)'.format(len(deleted)))
+            cmd_name = 'clear'
+            print('CMD [' + pfx + cmd_name + '] > ' + initiator_data)
+        elif message.content.startswith('debugtest'):
+            getruntime = round(time.time() - start_time, 10) / (60)
+            runtime = int(getruntime)
+            await self.send_message(message.channel, runtime)
+            await self.send_message(message.channel, commands)
+        if message.content.startswith(pfx):
+            await self.delete_message(message)
+bot = BacoPy()
+bot.run(token)
