b74cd5b04eb80ad1636735fe286c96febbe95ba8
HDR <refshet@gmail.com>
2019-02-16 06:22:30 +0100

    Continue making 2.0 better
    - Deletes messages for nsfw commands
    - Added Surprise

--- baco.py
+++ baco.py
@@ -8,23 +8,23 @@ from config import get_section
 extensions = get_section("bot").get("extensions")
 
 if __name__ == "__main__":
-    bot = commands.Bot(command_prefix=get_section("bot").get("command_prefix", "--"),pm_help=True)
+	bot = commands.Bot(command_prefix=get_section("bot").get("command_prefix", "--"),pm_help=True)
 
-    @bot.event
-    async def on_ready():
-        print('Connected!')
-        print(f'Username: {bot.user.name}')
-        print(f'ID: {bot.user.id}')
+	@bot.event
+	async def on_ready():
+		print('Connected!')
+		print(f'Username: {bot.user.name}')
+		print(f'ID: {bot.user.id}')
 		print(f'Version: 2.0')
-        await bot.change_presence(activity=Game(name="Browsing Reddit"))
+		await bot.change_presence(activity=Game(name="Browsing Reddit"))
 
-    for extension in extensions:
-        try:
-            bot.load_extension(extension)
-            print(f"Loaded extension {extension}")
-        except Exception as e:
-            print(f'Failed to load extension {extension}.')
-            traceback.print_exc()
+	for extension in extensions:
+		try:
+			bot.load_extension(extension)
+			print(f"Loaded extension {extension}")
+		except Exception as e:
+			print(f'Failed to load extension {extension}.')
+			traceback.print_exc()
 
-    print("Connecting to discord")
-    bot.run(get_section("bot").get("discord_token"))
+	print("Connecting to discord")
+	bot.run(get_section("bot").get("discord_token"))
--- cogs/reddit.py
+++ cogs/reddit.py
@@ -10,32 +10,42 @@ class Reddit(object):
 	
 	def __init__(self, bot):
 		self.bot = bot
-		self.last_images = deque(maxlen=10)
 		
 	@commands.command()
 	async def nsfw(self, ctx):
 		"""Grabs a random popular /r/nsfw post"""
 		await ctx.channel.trigger_typing()
 		nsfw = r.subreddit('nsfw').random()
-		await ctx.send(nsfw.url)
+		await ctx.message.delete()
+		await ctx.send(ctx.message.author.mention + " " + nsfw.url)
 	@commands.command()
 	async def gif(self, ctx):
 		"""Grabs a random popular /r/nsfw_gif or /r/HENTAI_GIF post"""
 		await ctx.channel.trigger_typing()
 		gif = r.subreddit('nsfw_gif+HENTAI_GIF').random()
-		await ctx.send(gif.url)
+		await ctx.message.delete()
+		await ctx.send(ctx.message.author.mention + " " + gif.url)
 	@commands.command()
 	async def r34(self, ctx):
 		"""Grabs a random popular /r/rule34 post"""
 		await ctx.channel.trigger_typing()
 		r34 = r.subreddit('rule34').random()
-		await ctx.send(gif.url)
+		await ctx.message.delete()
+		await ctx.send(ctx.message.author.mention + " " + r34.url)
 	@commands.command()
 	async def hentai(self, ctx):
 		"""Grabs a random popular /r/hentai post"""
 		await ctx.channel.trigger_typing()
 		hentai = r.subreddit('hentai').random()
-		await ctx.send(hentai.url)
-
+		await ctx.message.delete()
+		await ctx.send(ctx.message.author.mention + " " + hentai.url)
+	@commands.command()
+	async def surprise(self, ctx):
+		"""You're surprised by this command"""
+		await ctx.channel.trigger_typing()
+		surprise = r.subreddit('traphentai+delicioustraps+gonewild+nsfw+nsfw_gif+HENTAI_GIF+legalteens+ass+rule34+futanari+hugeboobs+funsized+nsfwcosplay+pornin15seconds+realgirls+asiansgonewild+biggerthanyouthought+petitegonewild+bustypetite+holdthemoan+trashyboners+hentai+ass+watchitfortheplot+milf+adorableporn+onoff+tittydrop+amateur+pawg+gonewildcurvy+happyembarrassedgirls+massivecock+cumsluts+girlsfinishingthejob+asstastic+curvy+gonewild30plus+ecchi+thick+anal+freeuse+bigboobsgw+blowjobs+workgonewild+festivalsluts+juicyasians+yiff+palegirls+stacked+altgonewild+tightdresses+nsfwhardcore+overwatch_porn').random()
+		await ctx.message.delete()
+		await ctx.send(ctx.message.author.mention + " " + surprise.url)
+		
 def setup(bot):
 	bot.add_cog(Reddit(bot))
